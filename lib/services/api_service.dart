cdaa59e798f2533ba4a7396809b6a758ae82af6a
Awaniass@26 <hawamaama002@gmail.com>
2025-05-09 16:36:50 +0000

    ip adress of my pc

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,19 +3,19 @@ import 'package:http/http.dart' as http;
 import '../models/student.dart';
 
 class ApiService {
-static const String baseUrl = 'http://localhost:3000/inscription';
+  static const String baseUrl = 'http://192.168.1.10:3000/inscription'; 
 
   static Future<List<Student>> fetchStudents({String? classe}) async {
     final uri = Uri.parse(classe != null
         ? '$baseUrl?classe=${Uri.encodeComponent(classe)}'
         : baseUrl);
-    final response = await http.get(uri);
 
+    final response = await http.get(uri);
     if (response.statusCode == 200) {
       final List<dynamic> data = jsonDecode(response.body);
       return data.map((json) => Student.fromJson(json)).toList();
     } else {
-      throw Exception('Échec du chargement des étudiants');
+      throw Exception('Échec du chargement des étudiants (${response.statusCode})');
     }
   }
 }
